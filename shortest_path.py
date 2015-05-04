24269ca8bdc023240b631e50f44024a9cc76a2fd
Yaphone <zhouyafeng2013@gmail.com>
2015-05-04 07:18:33 -0700

    s

--- shortest_path.py
+++ shortest_path.py
@@ -21,7 +21,7 @@ from ryu.ofproto import ofproto_v1_3
 from ryu.lib.packet import packet
 from ryu.lib.packet import ethernet
 from ryu.lib import hub
-import eventlet
+from operator import attrgetter
 
 from ryu.topology import event, switches
 from ryu.topology.api import get_switch, get_link
@@ -38,7 +38,10 @@ class SimpleSwitch13(app_manager.RyuApp):
         self.mac_to_port = {}
         self.topology_api_app=self
         self.net=nx.DiGraph()
-#        hub.spawn(self.show_info)
+        self.switches={}
+        self.links={}
+        self.switches_nodes={}
+        hub.spawn(self._monitor)
         
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
@@ -108,8 +111,8 @@ class SimpleSwitch13(app_manager.RyuApp):
             try:
                 path=nx.shortest_path(self.net, src, dst)
                 
-                print "*******Path**********"
-                print path
+#                print "*******Path**********"
+#                print path
                 
                 next=path[path.index(dpid)+1]
                 out_port=self.net[dpid][next]['port']
@@ -136,31 +139,59 @@ class SimpleSwitch13(app_manager.RyuApp):
     @set_ev_cls(event.EventSwitchEnter)
     def get_topology_data(self, ev):
          switch_list = get_switch(self.topology_api_app, None)
-         switches=[switch.dp.id for switch in switch_list]
+         self.switches=[switch.dp.id for switch in switch_list]
          links_list = get_link(self.topology_api_app, None)
-         links=[(link.src.dpid,link.dst.dpid,{'port':link.src.port_no}) for link in links_list]
+         self.links=[(link.src.dpid,link.dst.dpid,{'port':link.src.port_no}) for link in links_list]
          
+         self.switches_nodes=[switch.dp for switch in switch_list]
          
-         self.net.add_nodes_from(switches)
-         self.net.add_edges_from(links)
+         self.net.add_nodes_from(self.switches)
+         self.net.add_edges_from(self.links)
          
          
 #    def get_edges_weight():
 
-    def show_info(self):
+    def _monitor(self):
         while True:
-            print "*********List of nodes*************"
-            print self.net.nodes()
-            print "*********List of edges*************"
-            print self.net.edges()
+#            for dp in self.switches_nodes:
+#                self._request_stats(dp)
+            self._get_links_weight()            
+                                
+#            print self.links
+#            print self.switches
+            hub.sleep(5)
             
-#            nx.draw(self.net)
-#            plt.show()
+         
+    def _request_stats(self,datapath,port_no=4294967295):
+        self.logger.debug('send stats request: %016x', datapath.id)
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        
+        
+        req = parser.OFPPortStatsRequest(datapath, 0, port_no)
+        datapath.send_msg(req)
+        
+    @set_ev_cls(ofp_event.EventOFPPortStatsReply, MAIN_DISPATCHER)
+    def _port_stats_reply_handler(self, ev):
+        body = ev.msg.body
+        
+        self .logger.info('datapath              port '
+                          'rx-pkts  rx-bytes  rx-error '
+                          'tx-pkts  tx-bytes  tx-error')
+        self .logger.info('----------------  -------- '
+                          '-------- -------- -------- '
+                          '-------- -------- --------')
+        for stat in sorted(body, key=attrgetter('port_no')):
+            self .logger.info('%016x %8x %8d %8d %8d %8d %8d %8d',
+                              ev.msg.datapath.id, stat.port_no ,
+                              stat.rx_packets, stat.rx_bytes , stat.rx_errors ,
+                              stat.tx_packets, stat.tx_bytes , stat.tx_errors)
+#            edgewidth=[]
             
-            hub.sleep(10)
-         
-         
+            
+    def _get_links_weight(self):
+        for (u,v,d) in self.links:
+            self._request_stats(self.switches_nodes[u-1], port_no=d['port'])
 
-                
             
          
